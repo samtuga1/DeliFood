3367d485b858b045b5fd9e6127a1cf38dec2cf03
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-16 14:41:55 +0000

    Changed my initial Route to categories_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/categories_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const CategoriesScreen(),
     );
   }
 }
