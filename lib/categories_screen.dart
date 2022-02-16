639a29d7d555723bb2b34cc1b5f6404b06af8c9e
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-16 14:43:40 +0000

    Added the content of my category screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import './dummy-data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('DeliFood'),
+        ),
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+          ),
+          children: DUMMY_CATEGORIES.map((catData) {
+            return CategoryItem(title: catData.title, color: catData.color);
+          }).toList(),
+        ));
   }
 }
