aa4b3f6ced0ad22794140ffef5a59b25de3f1214
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-16 15:18:49 +0000

    Added a style to my title

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -9,7 +9,10 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.subtitle1,
+      ),
       padding: EdgeInsets.all(15),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
