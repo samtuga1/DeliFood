d5d515af011aebe88ccd08a1f5fde234b165424d
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-16 15:18:24 +0000

    Added some theming to my main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              subtitle1: TextStyle(
+                  fontSize: 20,
+                  // fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold),
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
     );
